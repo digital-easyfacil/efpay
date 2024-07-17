7ae44aef39b8321203fdd863f844f77877ab7d6e
Michel Souza <msouza@easyfacil.com.br>
2024-07-17 19:53:58 -0300

    First commit

---
+++ src/main/java/br/com/efpay/efpay/EfpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.efpay.efpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/efpay/efpay/EfpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.efpay.efpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
